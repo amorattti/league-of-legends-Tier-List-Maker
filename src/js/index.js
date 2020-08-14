c6cfb4c25bd4ed9b4f47e7c18e0799f153caca8a
tobiasz18 <tobiaszpost@gmail.com>
2020-08-14 15:17:33 +0200

    package test

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
-const { arr } = require("./test");
+import html2canvas from 'html2canvas';
 
-console.log(arr())
+document.querySelector('button').onclick = () => html2canvas(document.querySelector("#capture")).then(canvas => {
+  document.body.appendChild(canvas)
+});
