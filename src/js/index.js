2ba8bef920147a63b3494ea4cda2d216e81660ac
tobiasz18 <tobiaszpost@gmail.com>
2020-08-14 20:26:54 +0200

    add generate images from api / Dragula dnd / html2canvas

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
+import dragula from 'dragula';
 import html2canvas from 'html2canvas';
 
-document.querySelector('button').onclick = () => html2canvas(document.querySelector("#capture")).then(canvas => {
-  document.body.appendChild(canvas)
-});
+const container = document.querySelector('.tier-champions-container');
+const button = document.querySelector('button');
+
+async function getImages() {
+  const fetchApi = await fetch('http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json');
+  const fetchJson = await fetchApi.json();
+  Object.keys(fetchJson.data).map(champion => {
+    const myImage = new Image(80, 80);
+    myImage.src = `http://ddragon.leagueoflegends.com/cdn/10.16.1/img/champion/${champion}.png`;
+    container.appendChild(myImage);
+  });
+}
+
+async function getCanvas() {
+  const canvas = await html2canvas(document.querySelector(".tier-container"), {
+    useCORS: true,
+    backgroundColor: null,
+  });
+  document.querySelector('.screenshot-wrapper').style.display = 'flex';
+  document.querySelector('.screenshot-wrapper').appendChild(canvas);
+  console.log('hid')
+}
+getImages();
+
+
+const drake = dragula([].slice.call(document.querySelectorAll(".tier-sort")));
+drake.containers.push(container);
+
+button.addEventListener('click', () => getCanvas());
+console.log(document.querySelector('article'))
+document.querySelector('article').onClick = () => console.log('lalalalaalal')
