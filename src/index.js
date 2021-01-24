2a5b49be8e03cd9442675f2d4c1ccd560e22eebd
tobiasz18 <tobiaszpost@gmail.com>
2021-01-24 16:19:46 +0100

    Add auto update version

--- src/index.js
+++ src/index.js
@@ -6,51 +6,56 @@ import { SettingsUI } from './modules_js/SettingsUI';
 import { Settings } from './modules_js/Settings';
 import { fetchImages as fetchVersion } from './modules_js/fetch.images';
 
-// download the current chempions roster version
-let version = '10.23.1';
-fetchVersion('https://ddragon.leagueoflegends.com/api/versions.json').then(
-  (data) => (version = data[0]),
-);
+(async () => {
+  let version = '10.23.1';
+  await fetchVersion(
+    'https://ddragon.leagueoflegends.com/api/versions.json',
+    // eslint-disable-next-line no-return-assign
+  ).then((data) => {
+    const [currentVersion] = data;
+    version = currentVersion;
+  });
 
-const tierSortsRow = [...document.querySelectorAll('.tier-sort')];
+  const tierSortsRow = [...document.querySelectorAll('.tier-sort')];
 
-const changeArrow = new ChangePositionTools();
-const clickableElements = new ClickableElements(version);
-const settingsArrowsUI = new ChangeRowPositionUI(
-  '.tier-row',
-  '.move_up',
-  '.move_down',
-);
+  const changeArrow = new ChangePositionTools();
+  const clickableElements = new ClickableElements(version);
+  const settingsArrowsUI = new ChangeRowPositionUI(
+    '.tier-row',
+    '.move_up',
+    '.move_down',
+  );
 
-const buttons = new ClickableElementsUI([
-  ['.btn-show', 'click'],
-  ['.btn-close', 'click'],
-  ['.btn-download', 'click'],
-  ['#category', 'change'],
-  ['.settings', 'click'],
-]);
-const layoutElements = new ClickableElementsUI([
-  ['.overlay', 'click'],
-  ['.screenshot-wrapper', 'click'],
-]);
-const settingsUI = new SettingsUI('.tier-row', '.settings');
-const settingsTools = new Settings();
+  const buttons = new ClickableElementsUI([
+    ['.btn-show', 'click'],
+    ['.btn-close', 'click'],
+    ['.btn-download', 'click'],
+    ['#category', 'change'],
+    ['.settings', 'click'],
+  ]);
+  const layoutElements = new ClickableElementsUI([
+    ['.overlay', 'click'],
+    ['.screenshot-wrapper', 'click'],
+  ]);
+  const settingsUI = new SettingsUI('.tier-row', '.settings');
+  const settingsTools = new Settings();
 
-buttons.subscribe((selector, event) => {
-  clickableElements.changeTool(selector, event, version);
-});
+  buttons.subscribe((selector, event) => {
+    clickableElements.changeTool(selector, event, version);
+  });
 
-layoutElements.subscribe((selector, event) => {
-  clickableElements.changeTool(selector, event);
-});
+  layoutElements.subscribe((selector, event) => {
+    clickableElements.changeTool(selector, event);
+  });
 
-settingsArrowsUI.subscribe((selectorName, item) => {
-  changeArrow.changeTool(selectorName, item);
-});
+  settingsArrowsUI.subscribe((selectorName, item) => {
+    changeArrow.changeTool(selectorName, item);
+  });
 
-settingsUI.subscribe((selector, row) => {
-  settingsTools.changeButton(selector, row, tierSortsRow);
-});
+  settingsUI.subscribe((selector, row) => {
+    settingsTools.changeButton(selector, row, tierSortsRow);
+  });
 
-const drake = dragula(tierSortsRow);
-drake.containers.push(document.querySelector('.tier-champions-container'));
+  const drake = dragula(tierSortsRow);
+  drake.containers.push(document.querySelector('.tier-champions-container'));
+})();
